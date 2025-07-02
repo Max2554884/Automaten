5a473cf0e8473d0d791413f47af796d762155eab
max2554884 <max.wurmbach@t-online.de>
2025-07-02 10:41:20 +0200

    ja

--- src/Parser.java
+++ src/Parser.java
@@ -1,4 +1,27 @@
 package src;
 
-public class Parser {
+import java.util.List;
+
+public class Parser
+{
+    private boolean fehler;
+}
+public Parser (List<Token> pTokenliste){
+
+}
+public Token nextToken(){
+
+}
+public boolean parse(){
+
+}
+public boolean pruefeS() {
+
+}
+public boolean pruefeA(){
+
+}
+public boolean pruefeB(){
+
+}
 }
