beead23fe11cff180a01fabe5369cb2edcc3fb9b
Max2554884 <max.wurmbach@frg.wwschool.de>
2025-07-08 12:27:13 +0200

    ja

--- src/Parser.java
+++ src/Parser.java
@@ -1,27 +1,77 @@
 package src;
 
-import java.util.List;
 
-public class  Parser
-{
+public class Parser {
     private boolean fehler;
-}
-public Parser (List<Token> pTokenliste){
+    private List<Token> tokenliste;
+    private Token aktuellesToken;
 
-}
-public Token nextToken(){
+    public Parser(List<Token> pTokenliste){
+        this.tokenliste = pTokenliste;
+        this.fehler = false;
 
-}
-public boolean parse(){
+    }
+    public Token nextToken(){
+        if (!tokenliste.hasAccess()) {
+            tokenliste.toFirst();  // erstes Token als Start setzen
+        } else {
+            tokenliste.next();     // nächstes Token
+        }
 
-}
-public boolean pruefeS() {
+        if (tokenliste.hasAccess()) {
+            return tokenliste.getContent();
+        } else {
+            return null;  // kein Token mehr
+        }
+    }
 
-}
-public boolean pruefeA(){
+    public boolean parse(){
+        tokenliste.toFirst();
+        return pruefaS();
 
-}
-public boolean pruefeB(){
+    }
+    private boolean pruefaS() {
+        // S → A B
+        if (!pruefaA()) {
+            return false;
+        }
+        if (!pruefaB()) {
+            return false;
+        }
+        return true;
+    }
 
-}
+    private boolean pruefaA() {
+        // A → ("la" | "le")*
+        Token token;
+        while (true) {
+            token = nextToken();
+            if (token == null) {
+                fehler = true; // Erwartet B (lu), aber Liste vorbei
+                return false;
+            }
+            String wert = token.getWert();
+            if (wert.equals("la") || wert.equals("le")) {
+                // Token akzeptiert, weiter machen (schleife)
+            } else {
+                return true;
+            }
+
+        }
+    }
+    public boolean pruefaB(){
+        Token token = nextToken();
+        if (token == null) {
+            fehler = true;
+            return false;
+        }
+        if (token.getWert().equals("lu")) {
+            return true;
+        } else {
+            fehler = true;
+            return false;
+        }
+    }
+
+
 }
--- src/Token.java
+++ src/Token.java
@@ -1,4 +1,28 @@
 package src;
 
 public class Token {
+
+    private String wert;
+    private String typ;
+
+    public Token(String pWert, String pTyp){
+        this.wert = pWert;
+        this.typ = pTyp;
+    }
+    public String getTyp() {
+        return typ;
+    }
+
+    public void setTyp(String typ) {
+        this.typ = typ;
+    }
+
+    public String getWert() {
+        return wert;
+    }
+
+    public void setWert(String wert) {
+        this.wert = wert;
+    }
+
 }
